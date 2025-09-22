01b50311c437abfb1a46edb3ad56742a12aea6a4
arakaki35-beep <miko.arakaki1989@gmail.com>
2025-09-23 00:12:36 +0900

    Express version

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const http = require('http'); //httpのモジュール？をインポート
-const server = http.createServer((req,res) => { //reqとresを調べる
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
-    res.write('<h1>Hello NodeJS</h1>');
-    res.end();
+const express = require('express'); //httpのモジュール？をインポート
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('<h1>Hello Express</h1>');
 });
 
 const port = process.env.PORT || 8080; //Render対応のため固定にしないらしい
-server.listen(port);
-console.log('Server listen on port' + port);
+app.listen(port, () => {
+    console.log('Server is running on port ${port}');
+});
+
+
+// const server = http.createServer((req,res) => { //reqとresを調べる
+//     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+//     res.write('<h1>Hello NodeJS</h1>');
+//     res.end();
+// });
+
+// const port = process.env.PORT || 8080; //Render対応のため固定にしないらしい
+// server.listen(port);
+// console.log('Server listen on port' + port);
